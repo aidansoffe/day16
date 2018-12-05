e191e73b0dc0b671717e0a2a386a7b8c938f6e77
Moon Munar <moonmunar@Moons-MBP.attlocal.net>
2018-12-04 22:39:23 -0600

    day16

---
+++ student.py
@@ -0,0 +1,7 @@
+class student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.is_on_probation = is_on_probation
+        self.gpa = gpa
